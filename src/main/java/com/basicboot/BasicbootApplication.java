0edadc0d58149b81b3e7b589ba82df017d5d6a2e
khm <98224004+dr94406@users.noreply.github.com>
2023-08-31 10:50:49 +0900

    [init] 기본 setting

---
+++ src/main/java/com/basicboot/BasicbootApplication.java
@@ -0,0 +1,13 @@
+package com.basicboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicbootApplication.class, args);
+    }
+
+}
