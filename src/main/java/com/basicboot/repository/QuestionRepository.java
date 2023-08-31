cea8f445800dcca605442ab43b87beb0cad52cb1
khm <98224004+dr94406@users.noreply.github.com>
2023-08-31 16:42:06 +0900

    [feat] 테스트를 위한 메서드 추가

--- src/main/java/com/basicboot/repository/QuestionRepository.java
+++ src/main/java/com/basicboot/repository/QuestionRepository.java
@@ -2,6 +2,10 @@ package com.basicboot.repository;
 
 import com.basicboot.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Question findBySubject (String subject);
+    Question findBySubjectAndContent (String subject, String content);
 }
