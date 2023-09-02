c2aa084132b302dd99b3d859616a04c25c991e54
MINNIT <dr94406@naver.com>
2023-09-02 21:37:00 +0900

    [chore] 베이스 엔티티 걷어내기

--- src/main/java/com/basicboot/entity/Question.java
+++ src/main/java/com/basicboot/entity/Question.java
@@ -5,30 +5,41 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Question extends TimeBaseEntity{
+public class Question{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @Column(length = 200, nullable = false)
+    @Column(length = 200)
     private String subject;
 
-    @Column(columnDefinition = "TEXT" , nullable = false)
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList = new ArrayList<>();
 
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
     @Builder
-    Question(long id, String subject, String content){
+    Question(long id, String subject, String content, LocalDateTime createDate, LocalDateTime modifiedDate){
         this.id = id;
         this.subject = subject;
         this.content = content;
+        this.createDate = createDate;
+        this.modifiedDate = modifiedDate;
     }
 }
