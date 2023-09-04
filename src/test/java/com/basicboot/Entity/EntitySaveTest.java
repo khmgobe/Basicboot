217683498a917ffcd46867faa06ed4259a7441a5
khm <98224004+dr94406@users.noreply.github.com>
2023-09-04 11:17:36 +0900

    [chore] 작성 및 수정 추가

--- src/test/java/com/basicboot/Entity/EntitySaveTest.java
+++ src/test/java/com/basicboot/Entity/EntitySaveTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.LocalDateTime;
+
 @EnableJpaAuditing
 @SpringBootTest
 public class EntitySaveTest {
@@ -19,12 +21,16 @@ public class EntitySaveTest {
         Question question = Question.builder()
                 .subject("스프링 부트는 뭐야?")
                 .content("스프링 부트! 친해지자 ! ")
+                .createDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
                 .build();
         questionRepository.save(question);
 
         Question question2 = Question.builder()
                 .subject("스프링 부트 반가워")
                 .content("스프링 부트 행복해")
+                .createDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
                 .build();
 
         questionRepository.save(question2);
