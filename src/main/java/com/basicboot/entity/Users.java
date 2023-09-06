a5e613f59f9e5d2a4cf33364246b6e298fb15b8c
MINNIT <dr94406@naver.com>
2023-09-06 23:32:05 +0900

    [feat] 유저 엔티티 작성

---
+++ src/main/java/com/basicboot/entity/Users.java
@@ -0,0 +1,33 @@
+package com.basicboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Builder
+    public Users(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
