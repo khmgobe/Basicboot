9c85f1ddbd6cc71916cc83f4b1465b0b0c810e20
MINNIT <dr94406@naver.com>
2023-09-06 23:37:25 +0900

    [test] 테스트 코드 작성

--- src/test/java/com/basicboot/BasicbootApplicationTests.java
+++ src/test/java/com/basicboot/BasicbootApplicationTests.java
@@ -1,14 +1,28 @@
 package com.basicboot;
 
+import com.basicboot.Service.QuestionService;
 import com.basicboot.entity.Question;
 import com.basicboot.repository.QuestionRepository;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
 @EnableJpaAuditing
 class BasicBootApplicationTests {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Test
+    void testJpa() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            questionService.create(subject, content, null);
+        }
+    }
 
 }
