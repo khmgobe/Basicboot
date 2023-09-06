719d1bd3e9bfda206a2d8d4438c69a3f525f28d0
MINNIT <dr94406@naver.com>
2023-09-06 23:35:06 +0900

    [feat] 유저 시큐리티 추가 및 권한에 따른 분기처리

---
+++ src/main/java/com/basicboot/Service/UserSecurityService.java
@@ -0,0 +1,40 @@
+package com.basicboot.Service;
+
+import com.basicboot.entity.Users;
+import com.basicboot.repository.UserRepository;
+import com.basicboot.role.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Users> _users = this.userRepository.findByUsername(username);
+        if (_users.isEmpty()) {
+            throw new UsernameNotFoundException("사용자를 찾을수 없습니다.");
+        }
+        Users users = _users.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        if ("admin".equals(username)) {
+            authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
+        } else {
+            authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
+        }
+        return new User(users.getUsername(), users.getPassword(), authorities);
+    }
+}
