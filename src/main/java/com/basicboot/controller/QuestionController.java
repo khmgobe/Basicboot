0ddf3c98e226ad54e31c1a51178f73aef4a9646e
MINNIT <dr94406@naver.com>
2023-09-06 23:30:22 +0900

    [feat] 질문 컨트롤러 밸리데이션 적용

--- src/main/java/com/basicboot/controller/QuestionController.java
+++ src/main/java/com/basicboot/controller/QuestionController.java
@@ -1,13 +1,22 @@
 package com.basicboot.controller;
 
 import com.basicboot.Service.QuestionService;
+import com.basicboot.Service.UserService;
 import com.basicboot.entity.Question;
+import com.basicboot.entity.Users;
+import com.basicboot.form.AnswerForm;
+import com.basicboot.form.QuestionForm;
 import com.basicboot.repository.QuestionRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -16,29 +25,37 @@ import java.util.List;
 public class QuestionController {
 
     private final QuestionService questionService;
+    private final UserService userService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Question> paging = questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Long id) {
+    public String detail(Model model, @PathVariable("id") Long id, AnswerForm answerForm) {
         Question question = questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
     }
 
+    @PreAuthorize("isAuthenticated()")
+
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
-    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
-        questionService.create(subject, content);
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal) {
+        if(bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Users users = userService.getUser(principal.getName());
+        questionService.create(questionForm.getSubject(), questionForm.getContent(), users);
         return "redirect:/question/list"; // 질문 저장 후 목록으로 이동
     }
 }
