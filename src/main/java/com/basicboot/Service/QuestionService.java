0b1700013bf6578c6c7a9b41f7ce9eef4e3e4394
MINNIT <dr94406@naver.com>
2023-09-06 23:34:43 +0900

    [feat] 생성자 수정 및 페이징 추가

--- src/main/java/com/basicboot/Service/QuestionService.java
+++ src/main/java/com/basicboot/Service/QuestionService.java
@@ -1,12 +1,18 @@
 package com.basicboot.Service;
 
 import com.basicboot.entity.Question;
+import com.basicboot.entity.Users;
 import com.basicboot.exception.DataNotFoundException;
 import com.basicboot.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,13 +34,21 @@ public class QuestionService {
             throw new DataNotFoundException("question not found");
         }
     }
-    public void create(String subject, String content) {
+    public void create(String subject, String content, Users users) {
         Question q = new Question();
         Question question = Question.builder()
                 .subject(subject)
                 .content(content)
                 .createDate(LocalDateTime.now())
+                .author(users)
                 .build();
         questionRepository.save(question);
+    }
+
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return questionRepository.findAll(pageable);
     }
 }
